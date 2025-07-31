728cdeeccf74c2ea0835676af6dbe42fc63681d7
HeeLoco <66466597+HeeLoco@users.noreply.github.com>
2025-07-31 09:27:10 +0200

    Create user_assigned_managed_identity.tf

---
+++ user_assigned_managed_identity.tf
@@ -0,0 +1,5 @@
+module "avm-res-managedidentity-userassignedidentity" {
+  source  = "Azure/avm-res-managedidentity-userassignedidentity/azurerm"
+  version = "0.3.4"
+  # insert the 3 required variables here
+}
