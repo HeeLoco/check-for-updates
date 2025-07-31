c25127cc51187eaa24febcf6cfc201a6ac4638c3
HeeLoco <66466597+HeeLoco@users.noreply.github.com>
2025-07-31 09:24:14 +0200

    Update application_gateway.tf

--- application_gateway.tf
+++ application_gateway.tf
@@ -1,5 +1,5 @@
-module "application_gateway" {
-  depends_on       = [module.resourcegroup, module.virtual_network]
-  source           = "Azure/avm-res-network-applicationgateway/azurerm"
-  version          = "0.4.2"
+module "avm-res-network-applicationgateway" {
+  source  = "Azure/avm-res-network-applicationgateway/azurerm"
+  version = "0.4.2"
+  # insert the 9 required variables here
 }
