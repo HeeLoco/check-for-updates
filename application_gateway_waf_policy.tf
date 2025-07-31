3b4a6c77336c77c792db7e0d28ffbdd479dcadff
HeeLoco <66466597+HeeLoco@users.noreply.github.com>
2025-07-31 09:25:01 +0200

    Create application_gateway_waf_policy.tf

---
+++ application_gateway_waf_policy.tf
@@ -0,0 +1,5 @@
+module "avm-res-network-applicationgatewaywebapplicationfirewallpolicy" {
+  source  = "Azure/avm-res-network-applicationgatewaywebapplicationfirewallpolicy/azurerm"
+  version = "0.2.0"
+  # insert the 4 required variables here
+}
