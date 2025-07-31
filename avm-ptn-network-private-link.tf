9533ffc2b6edb457b9975dc253220a7811d84c35
HeeLoco <66466597+HeeLoco@users.noreply.github.com>
2025-07-31 14:09:39 +0200

    Create avm-ptn-network-private-link.tf

---
+++ avm-ptn-network-private-link.tf
@@ -0,0 +1,5 @@
+module "avm-ptn-network-private-link-private-dns-zones" {
+  source  = "Azure/avm-ptn-network-private-link-private-dns-zones/azurerm"
+  version = "0.16.0"
+  # insert the 2 required variables here
+}
